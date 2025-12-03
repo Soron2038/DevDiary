ac6aa000478f49e88bae06d7c6d653c0201a684e
Soron2038 <135825122+Soron2038@users.noreply.github.com>
2025-12-03 16:48:22 +0100

    Setup Swift Package structure with SwiftUI and SQLite.swift

---
+++ Sources/DevDiary/DevDiaryApp.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+@main
+struct DevDiaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Text("DevDiary - Initial Setup")
+                .frame(minWidth: 400, minHeight: 300)
+        }
+    }
+}
